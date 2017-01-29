0f5460c07ba4025c50a174fec2a2c98ed084e31d
vngoncharuk <v.n.goncharuk@gmail.com>
2017-01-29 22:09:41 +0200

    Initial commit

---
+++ src/main/java/com/vitaliy/practice/modul01/task011/FirstClass.java
@@ -0,0 +1,13 @@
+package com.vitaliy.practice.modul01.task011;
+
+/**
+ * Created by Vitaliy on 25.01.2017.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+
+        System.out.println("Мой первый класс на Java");
+
+    }
+
+}
