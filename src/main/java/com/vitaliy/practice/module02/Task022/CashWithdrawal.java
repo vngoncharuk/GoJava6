4b9e63730f05bb19f548fc952c827d5c7633aca4
vngoncharuk <v.n.goncharuk@gmail.com>
2017-02-06 21:32:39 +0200

    2.2 creating class constant for commission.

--- src/main/java/com/vitaliy/practice/module02/Task022/CashWithdrawal.java
+++ src/main/java/com/vitaliy/practice/module02/Task022/CashWithdrawal.java
@@ -4,21 +4,19 @@ package com.vitaliy.practice.module02.Task022;
  * Created by Vitaliy on 05.02.2017.
  */
 public class CashWithdrawal {
-
+    double comiss = 0.05;
     public static void main(String[] args) {
 
         double totalBalance = 100;
-        double summWithdrawal = 99;
+        double summWithdrawal = 10;
         CashWithdrawal transaction = new CashWithdrawal();
 
-        transaction.commissionForCash(totalBalance,summWithdrawal) ;
-
+        transaction.commissionForCash(totalBalance,summWithdrawal);
     }
     //
     public void commissionForCash(double balance, double summWithdrawal) {
         double summ = summWithdrawal;
         double totalbalance = balance;
-        double comiss = 0.05;
         comiss *= summ;
         if ((comiss + summWithdrawal) < totalbalance) {
             System.out.println("OK " + comiss + " " + (totalbalance - (comiss + summ)));
