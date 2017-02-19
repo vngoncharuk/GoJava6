308597f877302ccbecebc7bcff99309d033ccc0a
vitaliy <v.n.goncharuk@gmail.com>
2017-02-19 22:34:34 +0200

    Task 4.1

---
+++ src/main/java/com/vitaliy/practice/module04/Task041/EUBank.java
@@ -0,0 +1,33 @@
+package com.vitaliy.practice.module04.Task041;
+
+import com.vitaliy.practice.module04.Task042.Currency;
+
+/**
+ * Created by vitaliy on 19.02.2017.
+ */
+public class EUBank extends Bank {
+
+    public EUBank(long id, String bankCountry, Currency currency, int numberOfEmployees, double avrSalaryOfEmployee, long rating, long totalCapital) {
+        super(id, bankCountry, currency, numberOfEmployees, avrSalaryOfEmployee, rating, totalCapital);
+    }
+
+    @Override
+    public int getLimitOfWithdrawal() {
+        return 0;
+    }
+
+    @Override
+    public int getLimitOfFunding() {
+        return 0;
+    }
+
+    @Override
+    public int getMonthlyRate() {
+        return 0;
+    }
+
+    @Override
+    public int getCommission(int summ) {
+        return 0;
+    }
+}
